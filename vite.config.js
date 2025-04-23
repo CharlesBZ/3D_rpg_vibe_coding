ff5df040ab9ddcba86c13860cbf952da2aa74265
CharlesBZ <charles.a.bestjr@gmail.com>
2025-04-23 01:42:05 -0400

    B: add world, enemies, player

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
-    open: true, // Automatically open browser on server start
-    port: 3000, // Set default port
-  },
+  root: './',
+  publicDir: 'public',
+  base: process.env.NODE_ENV === 'production' ? '/threejs-vibecode-rpg/' : '/',
   build: {
-    outDir: 'dist', // Output directory for production build
-    assetsDir: 'assets', // Directory for static assets
-    minify: 'terser', // Minify with terser
-    sourcemap: true, // Generate source maps for debugging
+    outDir: 'docs',
   },
-  // Optimize dependencies
-  optimizeDeps: {
-    include: ['three'],
-  },
-  publicDir: 'public', // Specify the public assets directory
-}); 
+  server: {
+    host: true
+  }
+});
